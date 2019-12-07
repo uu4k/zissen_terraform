ae45b90a86fbffae32d29a1ba4f978a9c5cbb120
uu4k <t.shimizu1115@gmail.com>
2019-12-07 11:11:29 +0900

    add image build pipeline.

--- main.tf
+++ main.tf
@@ -160,3 +160,217 @@ module "example_sg" {
   port        = 80
   cidr_blocks = ["0.0.0.0/0"]
 }
+
+resource "aws_ecr_repository" "example" {
+  name = "example"
+}
+
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository = aws_ecr_repository.example.name
+
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 30 relase tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["release"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOF
+}
+
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage"
+    ]
+  }
+}
+
+module "codebuild_role" {
+  source     = "./iam_role"
+  name       = "codebuild"
+  identifier = "codebuild.amazonaws.com"
+  policy     = data.aws_iam_policy_document.codebuild.json
+}
+
+resource "aws_codebuild_project" "example" {
+  name         = "example"
+  service_role = module.codebuild_role.iam_role_arn
+  source {
+    type = "CODEPIPELINE"
+  }
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type            = "LINUX_CONTAINER"
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:2.0"
+    privileged_mode = true
+  }
+}
+
+data "aws_iam_policy_document" "codepipeline" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+      "ecs:DescribeServices",
+      "ecs:DescribeTaskDefinition",
+      "ecs:DescribeTasks",
+      "ecs:ListTasks",
+      "ecs:RegisterTaskDefinition",
+      "ecs:UpdateService",
+      "ima:PassRole"
+    ]
+  }
+}
+
+module "codepipeline_role" {
+  source     = "./iam_role"
+  name       = "codepipeline"
+  identifier = "codepipeline.amazonaws.com"
+  policy     = data.aws_iam_policy_document.codepipeline.json
+}
+
+resource "aws_s3_bucket" "artifact" {
+  bucket = "zissen-terraform-artifact"
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_codepipeline" "example" {
+  name     = "example"
+  role_arn = module.codepipeline_role.iam_role_arn
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = 1
+      output_artifacts = ["Source"]
+      configuration = {
+        Owner                = "uu4k",
+        Repo                 = "zissen_terraform_example_image"
+        Branch               = "master"
+        PollForSourceChanges = false
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = 1
+      input_artifacts  = ["Source"]
+      output_artifacts = ["Build"]
+      configuration = {
+        ProjectName = aws_codebuild_project.example.id
+      }
+    }
+  }
+
+  # TODO migrate等の前処理のはさみ方
+  # stage {
+  #   name = "Deploy"
+
+  #   action {
+  #     name            = "Deploy"
+  #     category        = "Deploy"
+  #     owner           = "AWS"
+  #     provider        = "ECS"
+  #     version         = 1
+  #     input_artifacts = ["Build"]
+  #     configuration = {
+  #       ClusterName = aws_ecs_cluster.example.name
+  #       ServiceName = aws_ecs_service.example.name
+  #       FileName    = "imagedefinitions.json"
+  #     }
+  #   }
+  # }
+
+  artifact_store {
+    location = aws_s3_bucket.artifact.id
+    type     = "S3"
+  }
+}
+
+resource "aws_codepipeline_webhook" "example" {
+  name = "example"
+  target_pipeline = aws_codepipeline.example.name
+  target_action = "Source"
+  authentication = "GITHUB_HMAC"
+  authentication_configuration {
+    secret_token = "FFjDIRVdBR"
+  }
+
+  filter {
+    json_path = "$.ref"
+    match_equals = "refs/heads/{Branch}" # aws_codepipelineのconfiguration.Branch参照
+  }
+}
+
+resource "github_repository_webhook" "example" {
+  repository = "zissen_terraform_example_image"
+  configuration {
+    url = aws_codepipeline_webhook.example.url
+    secret = "FFjDIRVdBR"
+    content_type = "json"
+    insecure_ssl = false
+  }
+
+  events = ["push"]
+}
